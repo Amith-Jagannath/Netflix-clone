20e7200f66de9b259dfb3e439eb9ab0aab63e3b3
Amith-Jagannath <amithjagannath02@gmail.com>
2023-06-13 23:47:34 +0530

    added banner section and styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Row from "./Row";
+import Banner from "./Banner";
 import requests from "./Requests";
 function App() {
   return (
     <div className="App">
-      <h1>Netflix clone frontend</h1>
+      <Banner />
+
       <Row
         title="NETFLIX ORIGINALS"
         fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
       />
       <Row title="Trending Now" fetchUrl={requests.fetchTrendings} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTrendings} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documenteries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -2,18 +2,17 @@ import React, { useEffect, useState } from "react";
 import axios from "./axios";
 import "./Row.css";
 const baseURL = "https://image.tmdb.org/t/p/original/";
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchUrl);
-      console.log(request);
+
       setMovies(request.data.results);
     }
     fetchData();
   }, [fetchUrl]);
 
-  console.log(movies);
   return (
     <div className="row">
       {/*title*/}
@@ -21,8 +20,11 @@ function Row({ title, fetchUrl }) {
       <div className="row__posters">
         {movies.map((movie) => (
           <img
-            className="row__poster"
-            src={`${baseURL}${movie.poster_path}`}
+            key={movie.id}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${baseURL}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
             alt={movie.name}
           />
         ))}
